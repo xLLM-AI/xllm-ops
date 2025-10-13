9d0cee31f221d391e63ec6e7544cd9168a5a6d58
lanliwei.1 <lanliwei.1@jd.com>
2025-10-13 08:18:35 +0800

    bugfix: remove multiple definition of main in the test of beam search.

--- test/beam_search_test.cpp
+++ test/beam_search_test.cpp
@@ -169,81 +169,81 @@
 //     }
 // }
 
-int main() {
+// int main() {
     
-    beam_search::BeamSearchBase inputs(512, 512, 2, 2);
-    inputs.create_torch_tensors();
-    beam_search::BeamSearchTorch op_torch(inputs);
-    op_torch.process();
-    cout<<"op_torch done"<<endl;
-    int deviceId = 0;
-    aclrtStream stream1;
-    utils::initialize_acl(deviceId, &stream1);
-    beam_search::BeamSearchOp op_cann(inputs);
-    op_cann.process(stream1);
-    aclrtSynchronizeStream(stream1);
+//     beam_search::BeamSearchBase inputs(512, 512, 2, 2);
+//     inputs.create_torch_tensors();
+//     beam_search::BeamSearchTorch op_torch(inputs);
+//     op_torch.process();
+//     cout<<"op_torch done"<<endl;
+//     int deviceId = 0;
+//     aclrtStream stream1;
+//     utils::initialize_acl(deviceId, &stream1);
+//     beam_search::BeamSearchOp op_cann(inputs);
+//     op_cann.process(stream1);
+//     aclrtSynchronizeStream(stream1);
     
-    cout<<"op_cann done"<<endl;
-    auto out_torch_cpu = inputs.output_token_ids_torch.to(torch::kCPU);
-    auto out_op_cpu = inputs.output_token_ids_op.to(torch::kCPU);
-    // cout<< "out torch:\n" << out_torch_cpu << endl;
-    // cout<< "out op:\n" << out_op_cpu << endl;
-    auto out_torch_index_cpu = inputs.output_token_index_torch.to(torch::kCPU);
-    auto out_op_index_cpu = inputs.output_token_index_op.to(torch::kCPU);
-    // cout<< "out index torch:\n" << out_torch_index_cpu << endl;
-    // cout<< "out index op:\n" << out_op_index_cpu << endl;
-    auto out_torch_log_cpu = inputs.output_log_probs_torch.to(torch::kCPU).view({-1, 1});
-    auto out_op_log_cpu = inputs.output_log_probs_op.to(torch::kCPU).view({-1, 1});
-    // cout<< "out log torch:\n" << out_torch_log_cpu << endl;
-    // cout<< "out log op:\n" << out_op_log_cpu << endl;
-    cout<< "token ids equal: " << torch::equal(out_torch_cpu, out_op_cpu) << endl;
-    cout<< "token index equal: " << torch::equal(out_torch_index_cpu,out_op_index_cpu) <<endl;
-    // cout << "token ids shape : " << out_torch_cpu.sizes() << endl;
-    // cout << "token index shape: " << out_torch_index_cpu.sizes() << endl;
-    // cout << "token log probs shape: " << out_torch_log_cpu.sizes() << endl;
-    auto token_ids_diff_mask = (out_torch_cpu != out_op_cpu);
-    auto token_index_diff_mask = (out_torch_index_cpu != out_op_index_cpu);
+//     cout<<"op_cann done"<<endl;
+//     auto out_torch_cpu = inputs.output_token_ids_torch.to(torch::kCPU);
+//     auto out_op_cpu = inputs.output_token_ids_op.to(torch::kCPU);
+//     // cout<< "out torch:\n" << out_torch_cpu << endl;
+//     // cout<< "out op:\n" << out_op_cpu << endl;
+//     auto out_torch_index_cpu = inputs.output_token_index_torch.to(torch::kCPU);
+//     auto out_op_index_cpu = inputs.output_token_index_op.to(torch::kCPU);
+//     // cout<< "out index torch:\n" << out_torch_index_cpu << endl;
+//     // cout<< "out index op:\n" << out_op_index_cpu << endl;
+//     auto out_torch_log_cpu = inputs.output_log_probs_torch.to(torch::kCPU).view({-1, 1});
+//     auto out_op_log_cpu = inputs.output_log_probs_op.to(torch::kCPU).view({-1, 1});
+//     // cout<< "out log torch:\n" << out_torch_log_cpu << endl;
+//     // cout<< "out log op:\n" << out_op_log_cpu << endl;
+//     cout<< "token ids equal: " << torch::equal(out_torch_cpu, out_op_cpu) << endl;
+//     cout<< "token index equal: " << torch::equal(out_torch_index_cpu,out_op_index_cpu) <<endl;
+//     // cout << "token ids shape : " << out_torch_cpu.sizes() << endl;
+//     // cout << "token index shape: " << out_torch_index_cpu.sizes() << endl;
+//     // cout << "token log probs shape: " << out_torch_log_cpu.sizes() << endl;
+//     auto token_ids_diff_mask = (out_torch_cpu != out_op_cpu);
+//     auto token_index_diff_mask = (out_torch_index_cpu != out_op_index_cpu);
 
-    auto token_ids_diff_indices = torch::nonzero(token_ids_diff_mask);
-    auto token_index_diff_indices = torch::nonzero(token_index_diff_mask);
+//     auto token_ids_diff_indices = torch::nonzero(token_ids_diff_mask);
+//     auto token_index_diff_indices = torch::nonzero(token_index_diff_mask);
 
-    // cout << "Token IDs different count: " << token_ids_diff_indices.size(0) << endl;
-    // cout << "Token Index different count: " << token_index_diff_indices.size(0) << endl;
+//     // cout << "Token IDs different count: " << token_ids_diff_indices.size(0) << endl;
+//     // cout << "Token Index different count: " << token_index_diff_indices.size(0) << endl;
     
-        // 打印不相等的索引和值
-    if (token_ids_diff_indices.size(0) > 0) {
-        // cout << token_ids_diff_indices <<endl;
-        auto torch_diff_vals = out_torch_cpu.index({token_ids_diff_mask});
-        auto op_diff_vals = out_op_cpu.index({token_ids_diff_mask});
+//         // 打印不相等的索引和值
+//     if (token_ids_diff_indices.size(0) > 0) {
+//         // cout << token_ids_diff_indices <<endl;
+//         auto torch_diff_vals = out_torch_cpu.index({token_ids_diff_mask});
+//         auto op_diff_vals = out_op_cpu.index({token_ids_diff_mask});
         
-        cout << "Token IDs differences:" << endl;
-        // cout << "  torch values at diff positions: " << torch_diff_vals << endl;
-        // cout << "  op values at diff positions: " << op_diff_vals << endl;
+//         cout << "Token IDs differences:" << endl;
+//         // cout << "  torch values at diff positions: " << torch_diff_vals << endl;
+//         // cout << "  op values at diff positions: " << op_diff_vals << endl;
     
-        auto diff_rows = token_ids_diff_indices.select(1, 0);
-        auto diff_cols = token_ids_diff_indices.select(1, 1);
-        auto torch_diff_log_vals = out_torch_log_cpu.index({diff_rows, diff_cols});
-        auto op_diff_log_vals = out_op_log_cpu.index({diff_rows, diff_cols});
-        // cout << "  torch log probs at diff positions: " << torch_diff_log_vals << endl;
-        // cout << "  op log probs at diff positions: " << op_diff_log_vals << endl;
-    }
+//         auto diff_rows = token_ids_diff_indices.select(1, 0);
+//         auto diff_cols = token_ids_diff_indices.select(1, 1);
+//         auto torch_diff_log_vals = out_torch_log_cpu.index({diff_rows, diff_cols});
+//         auto op_diff_log_vals = out_op_log_cpu.index({diff_rows, diff_cols});
+//         // cout << "  torch log probs at diff positions: " << torch_diff_log_vals << endl;
+//         // cout << "  op log probs at diff positions: " << op_diff_log_vals << endl;
+//     }
 
-    if (token_index_diff_indices.size(0) > 0) {
-        // cout << token_index_diff_indices<<endl;
-        auto torch_diff_vals = out_torch_index_cpu.index({token_index_diff_mask});
-        auto op_diff_vals = out_op_index_cpu.index({token_index_diff_mask});
-        cout << "Token Index differences:" << endl;
-        // cout << "  torch values at diff positions: " << torch_diff_vals << endl;
-        // cout << "  op values at diff positions: " << op_diff_vals << endl;
-    }
-    // cout<< "output_token_index torch:\n" <<out_torch_index_cpu<<endl;
-    cout<< "token log equal: " <<torch::equal(out_torch_log_cpu,out_op_log_cpu) <<endl;
-    op_cann.destroy_tensors();
-    aclrtDestroyStream(stream1);
-    aclrtResetDevice(deviceId);
-    aclFinalize();
-    return 0;
-}Id);
+//     if (token_index_diff_indices.size(0) > 0) {
+//         // cout << token_index_diff_indices<<endl;
+//         auto torch_diff_vals = out_torch_index_cpu.index({token_index_diff_mask});
+//         auto op_diff_vals = out_op_index_cpu.index({token_index_diff_mask});
+//         cout << "Token Index differences:" << endl;
+//         // cout << "  torch values at diff positions: " << torch_diff_vals << endl;
+//         // cout << "  op values at diff positions: " << op_diff_vals << endl;
+//     }
+//     // cout<< "output_token_index torch:\n" <<out_torch_index_cpu<<endl;
+//     cout<< "token log equal: " <<torch::equal(out_torch_log_cpu,out_op_log_cpu) <<endl;
+//     op_cann.destroy_tensors();
+//     aclrtDestroyStream(stream1);
+//     aclrtResetDevice(deviceId);
+//     aclFinalize();
+//     return 0;
+// }Id);
 //         aclFinalize();
 //     }
 // };
