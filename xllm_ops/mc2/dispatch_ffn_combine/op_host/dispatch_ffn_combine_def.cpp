6926989e114cb129a700bc12aaa9d55e03056bd2
ext.wangguangcai1 <ext.wangguangcai1@jd.com>
2026-07-14 14:30:29 +0800

    feat: Delete the source code of some built-in operators in CANN9.0

--- xllm_ops/mc2/dispatch_ffn_combine/op_host/dispatch_ffn_combine_def.cpp
+++ xllm_ops/mc2/dispatch_ffn_combine/op_host/dispatch_ffn_combine_def.cpp
@@ -91,6 +91,7 @@ class DispatchFFNCombine : public OpDef {
         .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel");
     this->AICore().AddConfig("ascend910_93", aicore_config);
     this->AICore().AddConfig("ascend910b", aicore_config);
+    this->AICore().AddConfig("ascend950", aicore_config);
     this->MC2().HcclGroup("group");
   }
 };
