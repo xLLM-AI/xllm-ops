50331b32506416153d96f91a939e2b73594d8473
ext.zhaoxingchen5 <ext.zhaoxingchen5@jd.com>
2026-07-24 10:28:12 +0800

    bugfix: fix add_rms_norm_bias bug in a5 platform

--- xllm_ops/moe/add_rms_norm_bias/op_kernel/add_rms_norm_bias_split_d.h
+++ xllm_ops/moe/add_rms_norm_bias/op_kernel/add_rms_norm_bias_split_d.h
@@ -351,7 +351,7 @@ private:
     __aicore__ inline void CopyOutRstd(uint32_t i_o_idx, uint32_t num)
     {
         LocalTensor<float> rstdLocal = outQueueRstd.DeQue<float>();
-#if __CCE_AICORE__ == 220 || (defined(__NPU_ARCH__) && __NPU_ARCH__ == 3003)
+#if (defined(__CCE_AICORE__) && (__CCE_AICORE__ == 220 || __CCE_AICORE__ == 310)) || (defined(__NPU_ARCH__) && __NPU_ARCH__ == 3003)
         DataCopyCustom<float>(rstdGm[i_o_idx * rowFactor], rstdLocal, num);
 #endif
         outQueueRstd.FreeTensor(rstdLocal);
