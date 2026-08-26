d49af1815faba5c3ed09cf4fb90c183e5efcd356
ext.zhaoxingchen5 <ext.zhaoxingchen5@jd.com>
2026-08-26 14:51:46 +0800

    feat: migrate quant_lightning_indexer_v2 and quant_lightning_indexer_v2_metadata to xllm-ops

---
+++ xllm_ops/attention/quant_lightning_indexer_v2/torch_extension/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["quant_lightning_indexer", "quant_lightning_indexer_metadata"]
+
+from .quant_lightning_indexer import (
+    quant_lightning_indexer,
+    quant_lightning_indexer_metadata,
+)
+from . import graph_convert_quant_lightning_indexer
